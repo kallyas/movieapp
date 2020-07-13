c065ed789bc3c36ffb582aaa1996a8dcacb8f822
Kally <kallyasmedia@gmail.com>
2020-07-13 10:51:36 +0000

    features

---
+++ src/features/index.js
@@ -0,0 +1,9 @@
+export { default as Auth } from "./Auth/Auth";
+
+export { default as Logout } from "./Auth/Logout/Logout";
+
+export { default as MovieBrowser } from "./MovieBrowser/MovieBrowser";
+
+export { default as Search } from "./Search/Search";
+
+export { default as Movie } from "./Movie/Movie";
